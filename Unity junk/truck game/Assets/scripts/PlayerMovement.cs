438b6519ee8c25f2c9aabb2f4a555252f13be066
Ethan Ware <10841219@my.uvu.edu>
2021-02-01 10:52:31 -0700

    made functioning player movements in the 2d environment
    you have to manually ad the vector2 force in the attribute editor

---
+++ Unity junk/truck game/Assets/scripts/PlayerMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody2D ballRigidbody2D;
+    public Vector2 direction;
+    public Vector2 ydirection;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            ballRigidbody2D.AddForce(-direction, ForceMode2D.Force);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            ballRigidbody2D.AddForce(direction, ForceMode2D.Force);
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            ballRigidbody2D.AddForce(ydirection, ForceMode2D.Force);
+        }
+    }
+}

d2003c51bc089b3b4c8341433da04c3937d6ba98
Ethan Ware <10841219@my.uvu.edu>
2021-02-08 23:12:11 -0700

    added text and tried to get the counter working bu collisions were jank

---
+++ Unity junk/truck game/Assets/scripts/PointCount.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PointCount : MonoBehaviour
+{
+    public int creditvalue = 3;
+    public int numbercount;
+    public Text countertext;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        numbercount += creditvalue;
+        countertext.text = numbercount.ToString();
+        gameObject.SetActive(false);
+    }
+}

f26d84341d914f4af7cf2b4f8d3d772fc5be42bc
Ethan Ware <10841219@my.uvu.edu>
2021-02-09 12:35:28 -0700

    got the point collection to work, added respawn on timer

--- Unity junk/truck game/Assets/scripts/PointCount.cs
+++ Unity junk/truck game/Assets/scripts/PointCount.cs
@@ -1,13 +1,13 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Timers;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class PointCount : MonoBehaviour
 {
-    public int creditvalue = 3;
-    public int numbercount;
+    public int numbercount, creditvalue = 3;
     public Text countertext;
 
     void OnTriggerEnter2D(Collider2D other)

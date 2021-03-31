d047fe08a6636923b2d632cb00c853b16f4d8f08
Ethan Ware <10841219@my.uvu.edu>
2021-03-31 00:22:14 -0600

    added player constraints

---
+++ Unity junk/Primitives Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerController : MonoBehaviour
+{
+    private float speed = 10.0f;
+    private float zBound = 6;
+    public Rigidbody playerRB;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRB = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveplayer();
+
+        constraints();
+    }
+    
+    // move player
+    void moveplayer()
+    {
+        float HorizontalInput = Input.GetAxis("Horizontal");
+        float VerticalInput = Input.GetAxis("Vertical");
+        
+        playerRB.AddForce(Vector3.forward * speed * VerticalInput);
+        playerRB.AddForce(Vector3.right * speed * HorizontalInput);
+    }
+    
+    //constraints
+    void constraints()
+    {
+        if (transform.position.z < -zBound)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, -zBound);
+        }
+        if (transform.position.z > zBound)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, zBound);
+        }
+    }
+}

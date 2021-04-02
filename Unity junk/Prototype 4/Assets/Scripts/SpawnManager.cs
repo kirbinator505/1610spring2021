a53bac80bbfa9b5acad9c8b8564ba7d84994decd
Ethan Ware <10841219@my.uvu.edu>
2021-04-02 15:10:36 -0600

    finished sumo game

--- Unity junk/Prototype 4/Assets/Scripts/SpawnManager.cs
+++ Unity junk/Prototype 4/Assets/Scripts/SpawnManager.cs
@@ -6,15 +6,26 @@ public class SpawnManager : MonoBehaviour
 {
     public GameObject enemyPrefab;
     private float spawnRange = 9;
+    public int enemyCount;
+    public int waveNumber = 1;
+    public GameObject powerupPrefab;
 
     void Start()
     {
-        Instantiate(enemyPrefab, GeneratePosition(), enemyPrefab.transform.rotation);
+        SpawnEnemyWave(waveNumber);
+        Instantiate(powerupPrefab, GeneratePosition(), powerupPrefab.transform.rotation);
     }
     
     void Update()
     {
-        
+        enemyCount = FindObjectsOfType<Enemy>().Length;
+
+        if (enemyCount == 0)
+        {
+            waveNumber++;
+            SpawnEnemyWave(waveNumber);
+            Instantiate(powerupPrefab, GeneratePosition(), powerupPrefab.transform.rotation);
+        }
     }
 
     private Vector3 GeneratePosition()
@@ -23,5 +34,13 @@ public class SpawnManager : MonoBehaviour
         float spawnPosZ = Random.Range(-spawnRange, spawnRange);
         Vector3 randomPos = new Vector3(spawnPosX, 0, spawnPosZ);
         return randomPos;
+    }
+
+    void SpawnEnemyWave(int enemiesToSpawn)
+    {
+        for (int i = 0; i < enemiesToSpawn; i++)
+        {
+            Instantiate(enemyPrefab, GeneratePosition(), enemyPrefab.transform.rotation);
+        }
     }
 }

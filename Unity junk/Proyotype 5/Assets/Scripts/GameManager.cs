13c09f5a20fa8a5240c2811268eb38a26117cfcb
Ethan Ware <10841219@my.uvu.edu>
2021-04-06 20:14:56 -0600

    completed the first two sections in prototype 5

---
+++ Unity junk/Proyotype 5/Assets/Scripts/GameManager.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+    public List<GameObject> targets;
+    public TextMeshProUGUI scoreText;
+
+    private float spawnRate = 1.0f;
+    private int score;
+
+    void Start()
+    {
+        StartCoroutine(SpawnTarget());
+        score = 0;
+        UpdateScore(0);
+    }
+    
+    void Update()
+    {
+        
+    }
+
+    IEnumerator SpawnTarget()
+    {
+        while (true)
+        {
+         yield return new WaitForSeconds(spawnRate);
+         int index = Random.Range(0, targets.Count);
+         Instantiate(targets[index]);
+        }
+    }
+
+    public void UpdateScore(int scoreToAdd)
+    {
+        score += scoreToAdd;
+        scoreText.text = "Score: " + score;
+    }
+}

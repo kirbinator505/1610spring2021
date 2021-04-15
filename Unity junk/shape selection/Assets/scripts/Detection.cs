e8433429f95b21da1e0fb7b61cab12aaa998faca
Ethan Ware <10841219@my.uvu.edu>
2021-04-15 02:17:43 -0600

    cleaned up a bunch of code

--- Unity junk/shape selection/Assets/scripts/Detection.cs
+++ Unity junk/shape selection/Assets/scripts/Detection.cs
@@ -1,30 +1,40 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class Detection : MonoBehaviour
 {
-    public IntData num;
     public StringData word;
+
+    private int number;
+    private GameObject shape;
+    
     private void Update()
     {
-        if (num.number == 1)
+        if (number == 1)
         { 
            word.shape = "the capsule";
         }
-        if (num.number == 2)
+        else if (number == 2)
         {
            word.shape = "the hexagon";
         }
-        if (num.number == 3)
+        else if (number == 3)
         {
            word.shape = "the square";
         }
-        else if (num.number == 0)
+        else if (number == 0)
         {
             word.shape = "nothing";
         }
+    }
+    
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        shape = other.gameObject;
+        number = shape.GetComponent<IntegerAssignment>().value;
+    }
+
+    private void OnTriggerExit2D()
+    {
+        number = 0;
     }
 }
--- Unity junk/truck game/Assets/scripts/CharacterMover.cs
+++ Unity junk/truck game/Assets/scripts/CharacterMover.cs
@@ -4,24 +4,21 @@ using UnityEngine;
 [RequireComponent(typeof(CharacterController))]
 public class CharacterMover : MonoBehaviour
 {
-    public float speed = 3f, gravity = -8f, jumpforce = 3f;
+    public float speed = 3f, gravity = -8f, jumpforce = 10f;
+    public CharacterController controller;
 
     private float ydirection;
     private int jumpcount, jumpcountmax = 2;
-    public CharacterController controller;
     private Vector3 movement, rotation;
 
     private void Start()
     {
-        controller.GetComponent<CharacterController>();
+        controller = GetComponent<CharacterController>();
     }
 
     void Update()
     {
         movement.Set(speed*Input.GetAxis("Vertical"), ydirection, 0);
-        rotation.Set(speed*Input.GetAxis("Horizontal"), ydirection, 0);
-
-
         ydirection += gravity * Time.deltaTime;
 
         if (controller.isGrounded && movement.y < 0)
@@ -36,6 +33,7 @@ public class CharacterMover : MonoBehaviour
             jumpcount++;
         }
 
+        rotation.y = Input.GetAxis("Horizontal");
         transform.Rotate(rotation);
         movement = transform.TransformDirection(movement);
         controller.Move(movement * Time.deltaTime);
